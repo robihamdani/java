de4c456616556774c91458dbfef9f36ebbf253fc
robihamdani <robi4us@gmail.com>
2022-05-18 06:44:00 -0700

    commit pertama kali

---
+++ src/main/java/com/robihm/mavenproject1/testClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.robihm.mavenproject1;
+
+/**
+ *
+ * @author TIPAR
+ */
+public class testClass {
+    
+}
